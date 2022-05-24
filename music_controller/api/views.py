0fffa22ea289b6612746463352d374378fafbaee
onticinc <onticinc@gmail.com>
2022-05-24 11:26:42 -0600

    debugged urls for api

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,8 @@
-from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
-
-def main(request):
-    return HttpResponse("Hello") 
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
