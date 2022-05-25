38a4d5b412efe4e1a2e838466ce9b804bc382fee
onticinc <onticinc@gmail.com>
2022-05-25 16:22:32 -0600

    create room view

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -8,3 +8,22 @@ from rest_framework.response import Response
 class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.guest_can_pause
+            votes_to_skip = serializer.data.votes_to_skip
+            host = self.request.session.session_key
+            queryset = room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
