3fd527333040a17bc28c155bfa548ebf908cb5b8
onticinc <onticinc@gmail.com>
2022-06-03 17:34:18 -0600

    serializer

--- api/views.py
+++ api/views.py
@@ -6,9 +6,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-# Create your views here.
-
-
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
