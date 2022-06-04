b7f050b57fbd8f38559fa32083d1bc4f7cdd50b5
onticinc <onticinc@gmail.com>
2022-06-04 15:59:40 -0600

    layout createroom

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -21,6 +21,24 @@ export default class CreateRoomPage extends Component {
   }
 
   render() {
-    return <Grid container spacing={1}></Grid> ;
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            Create a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+             <div align="center">Guest Control of Playback State</div>
+            </FormHelperText>
+            <RadioGroup row defaultValue='true'>
+              <FormLabelControl value="true" control={<Radio color="primary" />}/>
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
+    ) ;
   }
 }
