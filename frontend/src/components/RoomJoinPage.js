aae999938b230b91f66601002499d792c7b0406f
onticinc <onticinc@gmail.com>
2022-06-04 19:05:49 -0600

    revert room join

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,17 +1,4 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import { Link } from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-
-
-
 
 export default class RoomJoinPage extends Component {
   constructor(props) {
@@ -19,15 +6,6 @@ export default class RoomJoinPage extends Component {
   }
 
   render() {
-    return 
-    <div>
-      <Grid container spacing={1}>
-        <Grid item xs={12} align="center">
-          <Typography component="h4" variant="h4">
-            Room Join Page
-          </Typography>
-        </Grid>
-      </Grid>
-    </div>
+    return <p>This is the room join page</p>;
   }
 }
