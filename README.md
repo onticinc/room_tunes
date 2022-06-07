d87e2b37d2d76a09ecc535870eca61e88578633f
onticinc <onticinc@gmail.com>
2022-06-07 00:26:06 -0600

    fixed room join error

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # room_tunes
 
 Current video:
-https://www.youtube.com/watch?v=H9rHrlNTpq8&list=PLzMcBGfZo4-kCLWnGmK0jUBmGLaJxvi4j&index=7
+https://www.youtube.com/watch?v=ZP8ZMlKjT4o&list=PLzMcBGfZo4-kCLWnGmK0jUBmGLaJxvi4j&index=9
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -57,7 +57,7 @@ export default class RoomJoinPage extends Component {
         code: this.state.roomCode
       })
     };
-    fetch('/api/join-room/', requestOptions).then((response) => {
+    fetch('/api/join-room', requestOptions).then((response) => {
       if (response.ok) {
         this.props.history.push(`/room/${this.state.roomCode}`)
       } else {
