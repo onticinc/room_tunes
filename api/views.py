50fdfe9c4dd2d13739b6071529a4d4e45e41ab84
onticinc <onticinc@gmail.com>
2022-06-07 20:11:56 -0600

    edit room

--- api/views.py
+++ api/views.py
@@ -106,6 +106,8 @@ class UpdateRoom(APIView):
     serializer_class= UpdateRoomSerializer
 
     def patch(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             guest_can_pause = serializer.data.get('guest_can_pause')
@@ -115,6 +117,16 @@ class UpdateRoom(APIView):
             queryset = Room.objects.filter(code=code)
             if not queryset.exists():
                 return Response({'msg': 'Room not found.'}, status=status.HTTP_404_NOT_FOUND)
+            
+            room = queryset[0]
+            user_id = self.request.session.session_key
+            if room.host != user_id:
+                return Response({'msg': 'You are not the host of this room.'}, status=status.HTTP_403_FORBIDDEN)
+
+            room.guest_can_pause = guest_can_pause
+            room.votes_to_skip = votes_to_skip
+            room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
 
 
         return Response({'Bad Request': 'Invalid Data...'}, status=status.HTTP_400_BAD_REQUEST)
